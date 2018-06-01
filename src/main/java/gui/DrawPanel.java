3458cf46fab678956e9974c4a8c18bf8be061d0b
ZhangXiangxi <zxx_1996@qq.com>
2018-06-01 17:46:31 +0800

    Resize the two panels

--- src/main/java/gui/DrawPanel.java
+++ src/main/java/gui/DrawPanel.java
@@ -1,16 +1,23 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Xiangxi on 2018/6/1.
  * Contact him on xiangxi.zhang.cs@gmail.com
  */
 public class DrawPanel extends JPanel {
-    public DrawPanel() {
+    public final int WIDTH;
+    public final int HEIGHT;
+    public DrawPanel(int width, int height) {
         super();
+        WIDTH = width;
+        HEIGHT = height;
+        setBackground(Color.WHITE);
+        setSize(WIDTH, HEIGHT);
     }
-    public void drawMandelBrot() {
+    public void drawMandelbrot() {
         // TODO:
     }
 }
--- src/main/java/gui/MainGUI.java
+++ src/main/java/gui/MainGUI.java
@@ -10,26 +10,34 @@ import java.awt.*;
 public class MainGUI extends JFrame {
     private JPanel mainPanel = new JPanel();
     private JPanel controlPanel = new JPanel();
-    private DrawPanel drawPanel = new DrawPanel();
+    private DrawPanel drawPanel;
     public MainGUI() throws HeadlessException {
         super();
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         initialControlPanel();
-        mainPanel.setLayout(new GridLayout(1, 2));
-        mainPanel.add(drawPanel);
-        mainPanel.add(controlPanel);
+        initialMainPanel();
         this.setContentPane(mainPanel);
         this.setTitle("HIT计算机图形学实验 张翔熙");
     }
+    // TODO: reformat the function structures here
+    private void initialMainPanel() {
+        mainPanel.setLayout(null);
+        drawPanel = new DrawPanel(360, 340);
+        drawPanel.setBounds(0, 0, 360, 340);
+        mainPanel.add(drawPanel);
+        controlPanel.setBounds(360, 0, 90, 340);
+        mainPanel.add(controlPanel);
+    }
     private void initialControlPanel() {
-        controlPanel.setSize(90, 300);
+        controlPanel.setSize(90, 340);
         // TODO: size adjustment
+
         controlPanel.setBackground(Color.GRAY);
         controlPanel.setLayout(new GridLayout(1,1));
 
         JPanel buttonPanel = new JPanel();
         JButton drawMBButton = new JButton("Mandelbrot");
-        drawMBButton.addActionListener(e->drawPanel.drawMandelBrot());
+        drawMBButton.addActionListener(e->drawPanel.drawMandelbrot());
         buttonPanel.add(drawMBButton);
         controlPanel.add(buttonPanel);
     }
@@ -37,6 +45,7 @@ public class MainGUI extends JFrame {
         MainGUI mainGUI = new MainGUI();
         mainGUI.setVisible(true);
         mainGUI.setSize(450, 340);
+        mainGUI.setResizable(false);
         // TODO: size adjustment
     }
 }
