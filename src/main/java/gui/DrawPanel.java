0d13b150360a87f2aa175f4d41dcdaf3f3e6715c
ZhangXiangxi <zxx_1996@qq.com>
2018-06-01 18:26:11 +0800

    Test color paining with naive coloring function

--- src/main/java/gui/DrawPanel.java
+++ src/main/java/gui/DrawPanel.java
@@ -2,6 +2,7 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 /**
  * Created by Xiangxi on 2018/6/1.
@@ -10,14 +11,44 @@ import java.awt.*;
 public class DrawPanel extends JPanel {
     public final int WIDTH;
     public final int HEIGHT;
+    private Color[][] colors;
+
     public DrawPanel(int width, int height) {
         super();
         WIDTH = width;
         HEIGHT = height;
         setBackground(Color.WHITE);
         setSize(WIDTH, HEIGHT);
+        colors = new Color[WIDTH][HEIGHT];
     }
     public void drawMandelbrot() {
-        // TODO:
+        calculateMandelbrotColors();
+        drawColors();
+    }
+    private void calculateMandelbrotColors() {
+        for(int i = 0; i < WIDTH; i++) {
+            for(int j = 0; j <HEIGHT; j++) {
+                colors[i][j] = getMandelbrotColorAt(i, j);
+            }
+        }
+    }
+    private Color getMandelbrotColorAt(int i, int j) {
+        // TODO: put real generation function here
+        int iPosition = i / 4;
+        int jPosition = j / 4;
+        if ((iPosition + jPosition) % 2 == 0)
+            return Color.BLUE;
+        else
+            return Color.RED;
+    }
+    private void drawColors() {
+        for(int i = 0; i < WIDTH; i++)
+            for(int j = 0; j < HEIGHT; j++)
+                drawPointAt(i, j, colors[i][j]);
+    }
+    private void drawPointAt(int i, int j, Color color) {
+        Graphics graphics = this.getGraphics();
+        graphics.setColor(color);
+        graphics.drawLine(i, j, i, j+1);
     }
 }
