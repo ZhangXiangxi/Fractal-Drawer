6937282495cae5ac740a6512d733be58f2851760
ZhangXiangxi <zxx_1996@qq.com>
2018-06-02 12:41:42 +0800

    Generate Color Selector Architecture

---
+++ src/main/java/algorithms/ColorSelector.java
@@ -0,0 +1,11 @@
+package algorithms;
+
+import java.awt.*;
+
+/**
+ * Created by Xiangxi on 2018/6/2.
+ * Contact him on xiangxi.zhang.cs@gmail.com
+ */
+public interface ColorSelector {
+    public abstract Color getColor(double iterCount);
+}
---
+++ src/main/java/algorithms/GradualColorSelector.java
@@ -0,0 +1,32 @@
+package algorithms;
+
+import java.awt.*;
+
+/**
+ * Created by Xiangxi on 2018/6/2.
+ * Contact him on xiangxi.zhang.cs@gmail.com
+ */
+public class GradualColorSelector implements ColorSelector {
+    public final int MAXITERNUMBER;
+
+    public GradualColorSelector(int MAXITERNUMBER) {
+        this.MAXITERNUMBER = MAXITERNUMBER;
+    }
+
+    @Override
+    public Color getColor(double iterCount) {
+        Color colorLeft = Color.YELLOW;
+        Color colorRight = Color.CYAN;
+        float[] leftComponent = colorLeft.getRGBColorComponents(null);
+        float[] rightComponent = colorRight.getRGBColorComponents(null);
+        if (iterCount > 0) {
+            float ratio = (float)iterCount / (float)MAXITERNUMBER;
+            float[] finalColor = new float[3];
+            for(int i = 0; i < 3; i++)
+                finalColor[i] = leftComponent[i] + ratio * (rightComponent[i] - leftComponent[i]);
+            return new Color(finalColor[0], finalColor[1], finalColor[2]);
+        }
+        else
+            return Color.CYAN;
+    }
+}
